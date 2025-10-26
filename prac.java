1f25a6d81f8560a4b3d35e2b22a4970fabf697da
sushitha-19 <sushitha20@gmail.com>
2025-10-26 11:10:56 +0530

    Wipro1

---
+++ prac.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class prac {
+    public static void main(String args[]){
+        HashMap<String, Integer> h=new HashMap<>();
+        h.put("Alice",20);
+        h.put("ben",30);
+        h.put("lice",80);
+        h.put("Alice",60);
+        System.out.println(h);
+    }
+}
+
---
+++ prac.js
@@ -0,0 +1,4 @@
+console.log("Sushitha");
+var a=10;
+var b="10";
+console.log(a===b);
